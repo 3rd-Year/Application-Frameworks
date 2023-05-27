da3ea846f0276c6a6c6a13debb4d76cccb3c0afd
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2023-05-27 14:04:03 +0530

    Update api.js

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 
-// const apiConfig = axios.create({
-// 	baseURL: "https://ctms-api.vercel.app/",
-// 	headers: {
-// 		"Content-Type": "application/json",
-// 	},
-// });
-
-const apiConfig = axios.create({
-	baseURL: "https://localhost:5000/",
-	headers: {
-		"Content-Type": "application/json",
-	},
-});
-
+ const apiConfig = axios.create({
+	baseURL: "https://ctms-api.vercel.app/",
+ 	headers: {
+ 		"Content-Type": "application/json",
+ 	},
+ });
 
 export default apiConfig;
